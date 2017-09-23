f362cdde2db1288063ba023e78903d36dd7bfc9c
dpaulkinney <32225331+dpaulkinney@users.noreply.github.com>
2017-09-23 11:06:16 -0700

    Adding HelloWorld

--- Main/WeekOne.java
+++ Main/WeekOne.java
@@ -4,60 +4,22 @@ public class WeekOne {
 
 	}
 
-	/*
-	*
-	*Write a function that takes 2 ints
-	*Return a boolean
-	*Return true if the two ints are equal
-	*Return false if the two ints are not equal
-	*
-	*/
+	//Function which compares two values and returns true if equal
 	public static Boolean AreEqual(int x, int y){
-		if (x == y){
-			return true;
-		}
-		else {
-			return false;
-		}
+		return true;
 	}
 	
-	
-	/*
-	 *
-	 *Write a function that takes an int x and int y as input
-	 *
-	 *Your function should return the int value of x minus y
-	 */
-	
+	//Write a function that takes two ints and return the subtraction 
 	public static int Subtraction(int x, int y){
-		int returnInt = x-y;
-		return returnInt;
+		return 0;
 	}
 	
-	/*
-	 *
-	 *Write a function that takes that will take int x and int y as input
-	 *This function should swap the values of x and y
-	 *
-	 *Your function should return x
-	 */
-	
+	//Write a function that swaps the value of x and y
 	public static int Swap(int x, int y){
 		return x;
 	}
 	
-	/*
-	 *
-	 *Write a function that takes an input, n.
-	 *The program will print a string n times in the console
-	 *
-	 *Your function should return a the string concantenated together
-	 */
-	
-	/*
-	 * Write a function that will print 5 sentences about you on separate lines
-	 * This function will be called from the main function
-	 */
+	//Write a function that prints information about me.
 	public static void PrintAboutMe(){
 
 	}
