02fe266a253f59e120436bb2e00e24fe2b754ff3
Dustin <dpaulkinney@gmail.com>
2017-10-18 19:20:09 -0700

    Removing un-needed java files

--- src/MethodExample.java
+++ src/MethodExample.java
@@ -6,7 +6,6 @@ public class MethodExample {
 		MyMethod(); //Call MyMethod;
 	}
 	
-	
 	public static void MyMethod(){
 		System.out.println("Called my method");
 		//MyOtherMethod();
@@ -14,6 +13,18 @@ public class MethodExample {
 	
 	public static void MyOtherMethod(){
 		System.out.println("Called my other method");
+	}
+	
+	public static void HelloWorld(){
+		System.out.println(ReturnHello() + " "+ ReturnWorld()+ "!");
+	}
+	
+	public static String ReturnHello(){
+		return "Hello";
+	}
+	
+	public static String ReturnWorld(){
+		return "World";
 	}
 
 }
